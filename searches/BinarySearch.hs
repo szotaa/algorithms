586d71872c22676257e8faf31eb0a7e3bcd96fb5
szotaa <kubaszota97@gmail.com>
2018-08-15 17:13:34 +0200

    Add binary search algorithm

---
+++ searches/BinarySearch.hs
@@ -0,0 +1,11 @@
+binarySearch :: (Ord a) => a -> [a] -> Maybe Int
+
+binarySearch value [] = Nothing
+binarySearch value xs 
+    | midElement == value = Just midIndex
+    | midElement < value = fmap ((+1) . (+midIndex)) (binarySearch value (drop (midIndex + 1) xs)) 
+    | otherwise = binarySearch value (take midIndex xs)     
+        where midIndex = length xs `div` 2
+              midElement = xs !! midIndex
+          
+          
